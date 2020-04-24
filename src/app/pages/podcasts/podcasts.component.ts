d6b77c8d1acb04439c46b80a818fadba025a3115
fntkg <garces.g1999@gmail.com>
2020-04-24 14:42:01 +0200

    Funcionando boton de añadir podcasts a cola de reprpudccion

--- src/app/pages/podcasts/podcasts.component.ts
+++ src/app/pages/podcasts/podcasts.component.ts
@@ -3,6 +3,7 @@ import { Podcast } from 'src/app/podcast';
 import { PodcastService } from 'src/app/services/podcast.service';
 import { MessageService } from 'src/app/services/message.service';
 import { FormBuilder } from '@angular/forms';
+import { AudioService } from 'src/app/services/audio.service';
 
 @Component({
   selector: 'app-podcasts',
@@ -19,8 +20,10 @@ export class PodcastsComponent implements OnInit {
   description: string;
   checkoutForm;
   saved: boolean;
+  add: boolean;
+  added: boolean;
 
-  constructor(private podcastService: PodcastService, private messageService: MessageService, private formBuilder: FormBuilder) {
+  constructor(private podcastService: PodcastService, private messageService: MessageService, private formBuilder: FormBuilder, public audioService: AudioService) {
    }
 
   ngOnInit(): void {
@@ -51,7 +54,42 @@ export class PodcastsComponent implements OnInit {
   }
 
   // To save a podcast
-  toggleIcon(): void {
-   this.saved = !this.saved;
+  toggleIcon() {
+   this.saved = true;
+  }
+
+  addToPlayList(): void {
+    this.added = true;
+
+    // Comprobar que no haya algo ya reproduciendose
+    console.log(this.audioService.checkState());
+    if (this.audioService.checkState().playing){
+      for(let i=0; i<this.podcasts.episodes.length; i++){
+        var song = {
+          URL: this.podcasts.episodes[i].audio,
+          Nombre: this.podcasts.episodes[i].title,
+          Artistas: this.title_string[1],
+          Imagen: null,
+          ID: undefined,
+          Album: undefined
+        };
+        this.audioService.addToQueue(song);
+      }
+    } else {
+      this.audioService.openPodcast(this.podcasts.episodes[0].audio,
+        this.podcasts.episodes[0].title, this.title_string[1]);
+      for(let i=1; i<this.podcasts.episodes.length; i++){
+        var song = {
+          URL: this.podcasts.episodes[i].audio,
+          Nombre: this.podcasts.episodes[i].title,
+          Artistas: this.title_string[1],
+          Imagen: null,
+          ID: undefined,
+          Album: undefined
+        };
+        this.audioService.addToQueue(song);
+      }
+    }
+
   }
 }
