94c4445584bd07e4bcfdcdcbd5837df726821de6
Alvarogd6 <760704@unizar.es>
2020-05-10 16:36:57 +0200

    Minor fix

--- src/app/pages/toolbar/toolbar.component.ts
+++ src/app/pages/toolbar/toolbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CloudService } from 'src/app/services/cloud.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-toolbar',
@@ -9,10 +10,19 @@ import { CloudService } from 'src/app/services/cloud.service';
 export class ToolbarComponent implements OnInit {
 
   constructor(
-    public cloudService: CloudService
+    public cloudService: CloudService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
+  }
+
+  goTo() {
+    if(this.cloudService.user) {
+      this.router.navigateByUrl('/initial-screen');
+    } else {
+      this.router.navigateByUrl('');
+    }
   }
 
 }
